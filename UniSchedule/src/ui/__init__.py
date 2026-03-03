aad23524534e900358059b1c7ea49fba24db7cf0
Soufiane Zaari <134754270+SoufianeZaari@users.noreply.github.com>
2026-03-03 16:16:29 +0000

    Delete UniSchedule directory

--- UniSchedule/src/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/ui/__init__.py
-"""
-Package UI pour le système de gestion d'emploi du temps FSTT
-"""
-
-from src.ui.login_window import LoginWindow
-from src.ui.admin_window import AdminWindow
-__all__ = ['LoginWindow', 'AdminWindow']
